37e08c645b9a42a20b351edfa6b2787d1c11416c
Yang <122386241@qq.com>
2023-08-26 00:36:59 +0800

    美化界面

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,10 +1,26 @@
 return {
     {
-        'akinsho/bufferline.nvim',
-        version = "*",
-        -- dependencies = 'nvim-tree/nvim-web-devicons'
+        "akinsho/bufferline.nvim",
+        config = true, 
     },
     {
-        "lukas-reineke/indent-blankline.nvim" 
+        "lukas-reineke/indent-blankline.nvim",
+        config = true,
+    },
+    {
+        "lewis6991/gitsigns.nvim",
+        config = true,
+    },
+    {
+        "goolord/alpha-nvim",
+        config = function()
+            require'alpha'.setup(require'alpha.themes.dashboard'.config)
+        end
+    },
+    {
+        "RRethy/vim-illuminate",
+        config = function()
+            require('illuminate').configure()
+        end
     },
 }
